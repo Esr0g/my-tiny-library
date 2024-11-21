98a54757f6177fa649126ecb7390d7ae8bfae023
Esr0g <gorse.sacha@gmail.com>
2024-11-21 15:29:14 +0100

    Mise en place du serveur Eureka et du serveur de config

---
+++ config-server/src/test/java/com/my/tiny/library/config/server/ConfigServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.tiny.library.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
